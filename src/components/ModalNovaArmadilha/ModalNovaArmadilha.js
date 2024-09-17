576a9893b39038ed9da5fd3b28a7bd587e75fe9f
Luis Henrique <euluishenrique7@gmail.com>
2024-09-17 00:18:57 -0300

    Create armadilha e pasta, integração com a API

--- src/components/ModalNovaArmadilha/ModalNovaArmadilha.js
+++ src/components/ModalNovaArmadilha/ModalNovaArmadilha.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import { useState } from 'react';
 import './ModalNovaArmadilha.css';
 
 function ModalNovaArmadilha({ aberto, fechado }) {
+  const [formData, setFormData] = useState({
+    nome: '',
+    latitude: '',
+    longitude: '',
+  });
+
   if (!aberto) return null;
+
+  const handleFormEdit = (event, nome) => {
+    setFormData({
+      ...formData,
+      [nome]: event.target.value,
+    });
+  };
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3333/armadilhas`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),  
+      });
+
+      setFormData({ nome: '', latitude: '', longitude: '' });
+      fechado();
+      
+    } catch (err) {
+      console.error("Erro ao enviar os dados:", err);
+    }
+  };
 
   return (
     <div className="modalOverlay">
       <div className="conteudoModal">
         <h2>Nova Armadilha</h2>
-        <form>
+        <form onSubmit={handleForm}>
+
           <div className="form-group">
-            <label htmlFor="nome">Nome da Armadilha:</label>
-            <input required placeholder="Digite o nome da armadilha"/>
+            <label>Nome da Armadilha:</label>
+            <input required placeholder="Digite o nome da armadilha" value={formData.nome} onChange={(event) => handleFormEdit(event, 'nome')}/>
           </div>
 
           <div className="form-group">
-            <label htmlFor="latitude">Latitude:</label>
-            <input type="text" id="latitude" name="latitude" required placeholder="Digite a latitude do dispositivo"/>
+            <label>Latitude:</label>
+            <input required placeholder="Digite a latitude do dispositivo" value={formData.latitude} onChange={(event) => handleFormEdit(event, 'latitude')}/>
           </div>
 
           <div className="form-group">
-            <label htmlFor="longitude">Longitude:</label>
-            <input required placeholder="Digite a longitude do dispositivo"/>
+            <label>Longitude:</label>
+            <input required placeholder="Digite a longitude do dispositivo" value={formData.longitude} onChange={(event) => handleFormEdit(event, 'longitude')}/>
           </div>
 
-          <button type="submit" className='adicionar'>Adicionar</button>
-          <button onClick={fechado} className='cancelar'>Cancelar</button>
+          <button type="submit" className="adicionar">Adicionar</button>
+          <button type="button" onClick={fechado} className="cancelar">Cancelar</button>
         </form>
       </div>
     </div>
--- src/components/ModalNovaPasta/ModalNovaPasta.js
+++ src/components/ModalNovaPasta/ModalNovaPasta.js
@@ -1,35 +1,89 @@
-import React from 'react';
-import './ModalNovaPasta.css'; 
+import { useState, useEffect } from 'react';
+import './ModalNovaPasta.css';
 
 function ModalNovaPasta({ aberto, fechado }) {
+  const [formData, setFormData] = useState({
+    id_dispositivo: '',
+    nome_pasta: '',
+    id_pasta_drive: '',
+  });
+
+  const [dispositivos, setDispositivos] = useState([]); 
+
+  const handleFormEdit = (event, campo) => {
+    setFormData({
+      ...formData,
+      [campo]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    const fetchDispositivos = async () => {
+      try {
+        const response = await fetch('http://localhost:3333/armadilhas');
+        const data = await response.json();
+  
+        if (data.armadilhas && Array.isArray(data.armadilhas)) {
+          setDispositivos(data.armadilhas); 
+        } else {
+          console.error('Formato inesperado dos dados:', data);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar dispositivos:', error);
+      }
+    };
+  
+    fetchDispositivos();
+  }, []);
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3333/pastas', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      setFormData({ id_dispositivo: '', nome_pasta: '', id_pasta_drive: '' });
+      fechado();
+    } catch (err) {
+      console.error('Erro ao enviar os dados:', err);
+    }
+  };
+
   if (!aberto) return null;
 
   return (
     <div className="modalOverlay">
       <div className="conteudoModal">
         <h2>Nova Pasta</h2>
-        <form>   
+        <form onSubmit={handleForm}>
           <div className="form-group">
             <label>Nome da Pasta</label>
-            <input required placeholder="Digite o nome da pasta"/>
+            <input required placeholder="Digite o nome da pasta" value={formData.nome_pasta} onChange={(event) => handleFormEdit(event, 'nome_pasta')}/>
           </div>
 
           <div className="form-group">
             <label>Dispositivo associado</label>
-            <select required>
-              <option value="Dispositivo1">Dispositivo 1</option>
-              <option value="Dispositivo2">Dispositivo 2</option>
-              <option value="Dispositivo3">Dispositivo 3</option>
+            <select required onChange={(event) => handleFormEdit(event, 'id_dispositivo')} value={formData.id_dispositivo}>
+              <option value="">Selecione um dispositivo</option>
+              {dispositivos.map((dispositivo) => (
+                <option key={dispositivo.id} value={dispositivo.id}>
+                  {dispositivo.nome}
+                </option>
+              ))}
             </select>
           </div>
 
           <div className="form-group">
             <label>Link da pasta drive</label>
-            <input required placeholder="Exemplo: 'https://docs.google.com/presentation/d/149R4wwAGtXucabe8pwSi1gO1cUpJqJAtdUOa8hOLEeE/edit?usp=sharing'"/>
+            <input required placeholder="Exemplo: 'https://docs.google.com/presentation/d/149R4wwAGtXucabe8pwSi1gO1cUpJqJAtdUOa8hOLEeE/edit?usp=sharing'" value={formData.id_pasta_drive} onChange={(event) => handleFormEdit(event, 'id_pasta_drive')}/>
           </div>
 
-          <button type="submit" className='adicionar'>Adicionar</button>
-          <button onClick={fechado} className='cancelar'>Cancelar</button>
+          <button type="submit" className="adicionar"> Adicionar </button>
+          <button onClick={fechado} className="cancelar"> Cancelar </button>
         </form>
       </div>
     </div>
